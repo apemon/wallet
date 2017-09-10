01a1f732f194b64b4a9f315da726b67fb2c1b4a5
Parin Chiamananthapong <apemon642002@hotmail.com>
2017-09-11 00:56:35 +0700

    update ownership in token

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -7,9 +7,9 @@ contract Token is BasicToken, Ownable {
     string public name;
     string public symbol;
     uint public decimals;
-    uint public totalSupply;
 
     event Deposit(address indexed from, uint256 value, string txref);
+    event WithDraw(address indexed from, uint256 value, string txref);
 
     function Token(string _name, string _symbol, uint256 _decimals) {
         name = _name;
@@ -18,9 +18,17 @@ contract Token is BasicToken, Ownable {
     }
 
     function deposit(string _txref, uint _value) payable returns (uint256 amount) {
-        totalSupply.add(_value);
+        totalSupply = totalSupply.add(_value);
         balances[msg.sender] = balances[msg.sender].add(_value);
         Deposit(msg.sender, _value, _txref);
+        return balanceOf(msg.sender);
+    }
+
+    function withdraw(string _txref, uint _value) payable returns (uint256 amount) {
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        WithDraw(msg.sender, _value, _txref);
         return balanceOf(msg.sender);
     }
 }
--- contracts/zeppelin-solidity/ownership/Ownable.sol
+++ contracts/zeppelin-solidity/ownership/Ownable.sol
@@ -7,15 +7,18 @@ pragma solidity ^0.4.11;
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-  address public owner;
+  mapping(address => bool) owners;
+  address root;
 
+  event ChangeOwner(address indexed from, address indexed to, address indexed by); 
 
   /**
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
   function Ownable() {
-    owner = msg.sender;
+    owners[msg.sender] = true;
+    root = msg.sender;
   }
 
 
@@ -23,18 +26,42 @@ contract Ownable {
    * @dev Throws if called by any account other than the owner.
    */
   modifier onlyOwner() {
-    require(msg.sender == owner);
+    require(owners[msg.sender]);
     _;
   }
-
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
    * @param newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) onlyOwner {
-    require(newOwner != address(0));      
-    owner = newOwner;
+    _addOwnership(newOwner);
+    _removeOwnership(msg.sender);
+    ChangeOwner(msg.sender, newOwner, msg.sender);
+  }
+
+  function addOwnership(address newOwner) onlyOwner {
+    _addOwnership(newOwner);
+    ChangeOwner(address(0), newOwner, msg.sender);
+  }
+
+  function removeOwnership(address oldOwner) onlyOwner {
+    _removeOwnership(oldOwner);
+    ChangeOwner(oldOwner, address(0), msg.sender);
+  }
+
+  function _addOwnership(address newOwner) internal {
+    require(newOwner != address(0));
+    owners[newOwner] = true;
+  }
+
+  function _removeOwnership(address oldOwner) internal {
+    require(oldOwner != address(0));
+    owners[oldOwner] = false;
+  }
+
+  function isOwner(address _address) constant returns (bool) {
+    return owners[_address];
   }
 
 }
