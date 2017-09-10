96f7448db231acd3b81dec63c1119619744aae60
Parin Chiamananthapong <apemon642002@hotmail.com>
2017-09-11 01:39:44 +0700

    refractor some code and new statusable for token (status is not tested)

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -1,12 +1,15 @@
 pragma solidity ^0.4.11;
 import '../zeppelin-solidity/math/SafeMath.sol';
-import '../zeppelin-solidity/ownership/Ownable.sol';
 import '../zeppelin-solidity/token/BasicToken.sol';
+import '../ownership/Ownable.sol';
+import '../lifecycle/Statusable.sol';
 
-contract Token is BasicToken, Ownable {
+contract Token is BasicToken, Statusable {
     string public name;
     string public symbol;
     uint public decimals;
+
+    mapping(address => bool) frozenAccount;
 
     event Deposit(address indexed from, uint256 value, string txref);
     event WithDraw(address indexed from, uint256 value, string txref);
@@ -17,18 +20,19 @@ contract Token is BasicToken, Ownable {
         decimals = _decimals; 
     }
 
-    function deposit(string _txref, uint _value) payable returns (uint256 amount) {
+    function deposit(string _txref, uint _value) returns (uint256 amount) {
         totalSupply = totalSupply.add(_value);
         balances[msg.sender] = balances[msg.sender].add(_value);
         Deposit(msg.sender, _value, _txref);
         return balanceOf(msg.sender);
     }
 
-    function withdraw(string _txref, uint _value) payable returns (uint256 amount) {
+    function withdraw(string _txref, uint _value) returns (uint256 amount) {
         // SafeMath.sub will throw if there is not enough balance.
         balances[msg.sender] = balances[msg.sender].sub(_value);
         totalSupply = totalSupply.sub(_value);
         WithDraw(msg.sender, _value, _txref);
         return balanceOf(msg.sender);
     }
+
 }
